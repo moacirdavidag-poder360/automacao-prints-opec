296fea39aabcf8dc0cdb8750b489676a126a51a1
moacirdavidag <moacirdavidag@gmail.com>
2026-03-20 16:33:36 -0300

    feat: Tirar print em desenvolvimento

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,12 +1,14 @@
 import cron from "node-cron";
 import logger from "./logger.config.js";
+import takeScreenshotsService from "../services/screenshots/take-screenshots.service.js";
 
 const setupCronPrints = () => {
-  cron.schedule("0 */5 * * * *", async () => {
+  cron.schedule("0 */1 * * * *", async () => {
     try {
       logger.info(
         "Serviço do cron que vai tirar prints do Poder360 no futuro iniciado e rodando! :D"
       );
+      takeScreenshotsService();
     } catch (error) {
       if (error instanceof Error) {
         logger.error(`Erro ao iniciar o cron de tirar prints: ${error.message}`);
